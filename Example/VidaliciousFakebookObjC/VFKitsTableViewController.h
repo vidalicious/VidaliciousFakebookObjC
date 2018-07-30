906395dcbe4aa1fc721157b944c69f50ac127a3f
vidalicious <vidalicious@sina.com>
2018-07-31 00:17:10 +0800

    Dev aa

---
+++ Example/VidaliciousFakebookObjC/VFKitsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  VFKitsTableViewController.h
+//  VidaliciousFakebookObjC_Example
+//
+//  Created by vidalicious on 2018/7/30.
+//  Copyright © 2018 vidalicious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VFKitsTableViewController : UITableViewController
+
+@end
