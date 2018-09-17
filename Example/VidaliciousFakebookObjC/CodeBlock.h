f77fbdef7a047edb0c49a580e71826c72f8d9845
vidalicious <vida.gu@cootek.cn>
2018-09-17 20:11:08 +0800

    dev

---
+++ Example/VidaliciousFakebookObjC/CodeBlock.h
@@ -0,0 +1,13 @@
+//
+//  CodeBlock.h
+//  VidaliciousFakebookObjC_Example
+//
+//  Created by vidalicious on 2018/9/17.
+//  Copyright © 2018 vidalicious. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CodeBlock : NSObject
+
+@end
